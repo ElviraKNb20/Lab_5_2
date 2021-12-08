813286e85802c9f1829dcdb4d07ffa88d347fb33
Ельвіра Матраєва <e.matrayeva@kpnu.km.ua>
2021-12-08 18:33:09 +0300

    Добавьте файлы проекта.

---
+++ Lab_5_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab_5_2
+{
+    interface ITool
+    {
+        string KEY { set { KEY = "До мажор"; } }
+        void play();
+    }
+    class Guitar : ITool
+    {
+        public int quantity { get; set; }
+        public Guitar(int quantity)
+        {
+            this.quantity = quantity;
+        }
+        public void play()
+        {
+            Console.WriteLine($"Грає гiтара з кiлькiстю струн - {quantity}");
+        }
+    }
+    class Drum : ITool
+    {
+        public double size { get; set; }
+        public Drum(double size)
+        {
+            this.size = size;
+        }
+        public void play()
+        {
+            Console.WriteLine($"Грає барабан з розмiром - {size} см");
+        }
+    }
+    class Trumpet : ITool
+    {
+        public double diameter { get; set; }
+        public Trumpet(double diameter)
+        {
+            this.diameter = diameter;
+        }
+        public void play()
+        {
+            Console.WriteLine($"Грає труба з дiаметром - {diameter}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITool[] tools = { new Guitar(6), new Drum(45.5), new Trumpet(5) };
+            foreach (ITool t in tools)
+            {
+                t.play();
+            }
+        }
+    }
+}
